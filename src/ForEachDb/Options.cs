d894e851983bf5f80054e893799b75a8547d9f37
Kent Chenery <kent@chenery.net.nz>
2022-11-16 14:19:13 +1300

    Basic working prototype

--- src/ForEachDb/Options.cs
+++ src/ForEachDb/Options.cs
@@ -1,9 +1,8 @@
-﻿using JetBrains.Annotations;
+﻿using System.Runtime.InteropServices;
 
 namespace ForEachDb;
 using CommandLine;
     
-[UsedImplicitly(ImplicitUseTargetFlags.Members)]
 public class Options
 {
     [Option('h', "host", HelpText = "Hostname to connect to.", Default = "localhost")]
@@ -15,7 +14,7 @@ public class Options
     [Option('u', "username", HelpText = "Username for the connection", Default = "postgres")]
     public string? Username { get; set; } 
     
-    [Option('p', "password", HelpText = "Password for the connection")]
+    [Option('p', "password", Required = true, HelpText = "Password for the connection")]
     public string? Password { get; set; }
     
     [Option("port", HelpText = "Password for the connection", Default = 5432)]
@@ -23,13 +22,4 @@ public class Options
 
     [Option('q', "query", Required = true, HelpText = "Query to run against each database")]
     public string? Query { get; set; }
-    
-    [Option("ignore", HelpText = "List of databases that should be ignored")]
-    public IEnumerable<string>? IgnoreDatabases { get; set; }
-    
-    [Option("include-postgres-db", HelpText = "Flag to include the postgres database")]
-    public bool IncludePostgresDb { get; set; }
-    
-    [Option("include-template-db", HelpText = "Flag to include the postgres database")]
-    public bool IncludeTemplateDb { get; set; }
 }
--- src/ForEachDb/Program.cs
+++ src/ForEachDb/Program.cs
@@ -1,4 +1,5 @@
-﻿using CommandLine;
+﻿using System.Diagnostics;
+using CommandLine;
 using Dapper;
 using ForEachDb;
 using ForEachDbQueries;
@@ -6,58 +7,38 @@ using Npgsql;
 
 string connectionString = "";
 string query = "";
-List<string> ignoreDatabases = new();
 
-var dbFinder = new DatabaseFinder();
-
-// Parse arguments
+// Get connection
 Parser.Default.ParseArguments<Options>(args)
-    .WithParsed(options =>
+    .WithParsed<Options>(o =>
     {
-        if (options is null)
-        {
-            throw new ArgumentNullException(nameof(options),"Must specify options");
-        }
-        
-        // Handle required fields
-        options.Password ??= ReadLine.ReadPassword("Password: ");
-        
-        if (options.Query is not null) query = options.Query;
-        if (options.IgnoreDatabases is not null) ignoreDatabases.AddRange(options.IgnoreDatabases);
-        if (!options.IncludePostgresDb) dbFinder.IgnorePostgresDb();
-        if (!options.IncludeTemplateDb) dbFinder.IgnoreTemplateDb();
-        
-        foreach (var ignoreDb in ignoreDatabases)
-        {
-            dbFinder.IgnoreDatabase(ignoreDb);
-        }
+        if (o?.Query is not null) query = o.Query;
 
+        Debug.Assert(o != null, nameof(o) + " != null");
         var csBuilder = new NpgsqlConnectionStringBuilder
         {
-            Host = options.HostName,
-            Database = options.Database,
-            Username = options.Username,
-            Password = options.Password,
-            Port = options.Port
+            Host = o.HostName,
+            Database = o.Database,
+            Username = o.Username,
+            Password = o.Password,
+            Port = o.Port
         };
 
         connectionString = csBuilder.ToString();
     });
 
-// Find databases and run query against them
-if (!string.IsNullOrEmpty(connectionString))
+if (connectionString != "")
 {
     var connection = new NpgsqlConnection(connectionString);
     
-    var databases = (await connection.QueryAsync<string>(dbFinder.Query().RawSql, dbFinder.Query().Parameters)).Order().ToList();
+    Console.WriteLine("Finding databases...");
+    var dbFinder = new DatabaseFinder()
+        .IgnorePostgresDb()
+        .IgnoreTemplateDb()
+        .Query();
 
-    if (databases.Any())
-    {
-        var forEachDb = new ForEachDbRunner(connectionString, null);
-        await forEachDb.RunQueryAsync(databases, query);
-    }
-    else
-    {
-        Console.WriteLine("No databases found to run query against");
-    }
+    var databases = (await connection.QueryAsync<string>(dbFinder.RawSql, dbFinder.Parameters)).ToList().Order();
+
+    var forEachDb = new ForEachDbRunner(connectionString, null);
+    await forEachDb.RunQueryAsync(databases, query);
 }
