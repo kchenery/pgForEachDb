b2f10e814faa730e22d926e0757f90e7b320f009
Kent Chenery <kent@chenery.net.nz>
2023-03-28 23:51:54 +1300

    Remove OptionSettings class

--- src/ForEachDb/OptionSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace ForEachDb;
-
-public class OptionSettings
-{
-    public string ConnectionString { get; set; }
-    public string Query { get; set; }
-    public List<string> IgnoreDatabases { get; set; }
-}
